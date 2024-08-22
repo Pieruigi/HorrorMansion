e7e9319354e1b14703f2a5d0d1e24e8fde2c0794
Pieruigi <pierluigi.zomparelli@gmail.com>
2024-08-23 01:09:01 +0200

    burning controller completed

--- Assets/Scripts/Misc/BurningController.cs
+++ Assets/Scripts/Misc/BurningController.cs
@@ -1,3 +1,4 @@
+using AndreyGraphics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,22 +11,61 @@ namespace Kidnapped
         [SerializeField]
         float startBurningDuration = 5;
 
+        // Negative means infinite
         [SerializeField]
-        float stopBurningDuration = 30;
+        float burningDuration = 10;
+
+        
+        [SerializeField]
+        float stopBurningDuration = 0;
 
         [SerializeField]
         List<Renderer> renderers;
 
-        bool burning = false;
+        [SerializeField]
+        Mesh shapeMesh;
+
+        [SerializeField]
+        ParticleSystem fireParticleSystem;
+
+        [SerializeField]
+        float[] fireEmissionRateArray;
+
+        [SerializeField]
+        List<Light> lights;
+
+        [SerializeField]
+        List<AudioSource> audioSources;
+
+       
+        bool looping = false;
         float elapsed = 0;
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            //Shader.SetGlobalFloat("StartBurning", 0f);
-            //Shader.SetGlobalFloat("StopBurning", 0f);
+        ParticleSystem[] fireParticles;
+        
+        float[] maxVolumes;
+        float stopAudioTime = 3f;
 
+        float[] maxIntensities;
+        float stopLightTime = 3;
+
+        void Awake()
+        {
+            
+
+            // Reset shaders
             ResetBurningShaders();
+
+            // Init particle systems
+            InitFireParticles();
+
+            // Init lights
+            InitLights();
+
+            // Init audio
+            InitAudio();
+
+
         }
 
         // Update is called once per frame
@@ -38,22 +78,40 @@ namespace Kidnapped
             }
                         
 
-            if (burning)
+            if (looping)
             {
                 elapsed += Time.deltaTime;
 
-                if(elapsed < startBurningDuration)
+                // Start burning or keep burning
+                if((elapsed < startBurningDuration || startBurningDuration == 0) && (elapsed < startBurningDuration + burningDuration || burningDuration < 0))
                 {
-                    SetStartBurning(elapsed / startBurningDuration);
+                    if(elapsed < startBurningDuration || startBurningDuration == 0) // Start burning
+                    {
+                        float value = elapsed / startBurningDuration;
+                        SetStartBurning(startBurningDuration > 0 ? value : 1);
+                        PlayFireParticleSystem(startBurningDuration > 0 ? value : 1);
+                        PlayAudio(startBurningDuration > 0 ? value : 1);
+                        PlayLights(startBurningDuration > 0 ? value : 1);
+                    }
+                    else // Keep burning
+                    {
+                        PlayFireParticleSystem(burningDuration > 0 ? 1 - elapsed / (startBurningDuration + burningDuration) : 1);
+                    }    
                 }
-                else
+                else // Sto pburning
                 {
-                    SetStopBurning((elapsed - startBurningDuration) / stopBurningDuration);
-                    
+                    if(burningDuration >= 0 && elapsed > startBurningDuration + burningDuration)
+                    {
+                        SetStopBurning((elapsed - startBurningDuration - burningDuration) / stopBurningDuration);
+                        StopFireParticleSystem();
+                        StopAudio(1f - (elapsed - startBurningDuration - burningDuration) / stopAudioTime);
+                        StopLights(1f - (elapsed - startBurningDuration - burningDuration) / stopLightTime);
+                    }
+                        
                 }
 
-                if (elapsed > stopBurningDuration + startBurningDuration)
-                    burning = false;
+                if (burningDuration >= 0 && elapsed > stopBurningDuration + startBurningDuration + burningDuration)
+                    looping = false;
             }
 
         }
@@ -67,12 +125,172 @@ namespace Kidnapped
             }
         }
 
-        public void StartBurning()
+        void InitAudio()
         {
-            SetStartBurning(0);
-            burning = true;
-            elapsed = 0;
+            if (audioSources != null)
+            {
+                maxVolumes = new float[audioSources.Count];
+                for(int i=0; i<audioSources.Count; i++)
+                {
+                    maxVolumes[i] = audioSources[i].volume;
+                    audioSources[i].volume = 0;
+                }
+                
+                
+            }
         }
+
+        void PlayAudio(float normalizedVolume)
+        {
+            if (audioSources == null)
+                return;
+            normalizedVolume = Mathf.Clamp01(normalizedVolume);
+            for (int i=0; i<audioSources.Count;i++)
+            {
+                if (!audioSources[i].isPlaying)
+                    audioSources[i].Play();
+                audioSources[i].volume = normalizedVolume * maxVolumes[i];
+            }
+                
+        }
+
+        void StopAudio(float normalizedVolume)
+        {
+            
+            if (audioSources != null)
+            {
+                normalizedVolume = Mathf.Clamp01(normalizedVolume);
+                for (int i = 0; i < audioSources.Count; i++)
+                {
+                    if (audioSources[i].isPlaying)
+                    {
+                        audioSources[i].volume = maxVolumes[i] * normalizedVolume;
+                        if (normalizedVolume == 0)
+                            audioSources[i].Stop();
+                    }
+                    
+                }
+
+            }
+        }
+
+
+
+        void InitLights()
+        {
+            if (lights != null)
+            {
+                maxIntensities = new float[lights.Count];
+                for (int i = 0; i < lights.Count; i++)
+                {
+                    maxIntensities[i] = lights[i].intensity;
+                    lights[i].intensity = 0;
+                    lights[i].enabled = false;
+                    LightFlicker flicker = lights[i].GetComponent<LightFlicker>();
+                    UpdateLightFlicker(flicker, 0);
+                    flicker.enabled = false;
+                }
+            }
+        }
+
+        void PlayLights(float normalizedValue)
+        {
+            if(lights == null) return;
+            normalizedValue = Mathf.Clamp01(normalizedValue);
+            for (int i=0; i < lights.Count; i++)
+            {
+                LightFlicker flicker = lights[i].GetComponent<LightFlicker>();
+                if (!lights[i].enabled)
+                {
+                    lights[i].enabled = true;
+                    flicker.enabled = true;
+                }
+
+                lights[i].intensity = maxIntensities[i] * normalizedValue;
+                UpdateLightFlicker(flicker, lights[i].intensity);
+            }
+        }
+
+        void StopLights(float normalizedValue)
+        {
+            if (lights == null) return;
+            normalizedValue = Mathf.Clamp01(normalizedValue);
+            for (int i = 0; i < lights.Count; i++)
+            {
+                if (lights[i].enabled)
+                {
+                    LightFlicker flicker = lights[i].GetComponent<LightFlicker>();
+                    lights[i].intensity = maxIntensities[i] * normalizedValue;
+                    UpdateLightFlicker(flicker, lights[i].intensity);
+                    if (normalizedValue == 0)
+                    {
+                        Debug.Log($"StopLight:{lights[i].gameObject.name}");
+                        flicker.enabled = false;
+                        lights[i].enabled = false;
+                    }
+                         
+                }
+                    
+                    
+            }
+        }
+
+        void UpdateLightFlicker(LightFlicker flicker, float intensity)
+        {
+            flicker.minIntensity = intensity * .8f;
+            flicker.maxIntensity = intensity * 1.2f;
+        }
+
+        void InitFireParticles()
+        {
+            if (fireParticleSystem != null)
+            {
+                if (shapeMesh == null)
+                    shapeMesh = renderers[0].GetComponent<MeshFilter>().sharedMesh;
+
+                fireParticles = new ParticleSystem[fireParticleSystem.transform.childCount];
+                for (int i = 0; i < fireParticleSystem.transform.childCount; i++)
+                {
+                    fireParticles[i] = fireParticleSystem.transform.GetChild(i).GetComponent<ParticleSystem>();
+                    var shape = fireParticles[i].shape;
+                    Debug.Log($"Setting shapemesh:{shapeMesh}");
+                    shape.mesh = shapeMesh;
+                    
+                }
+            }
+        }
+
+        void PlayFireParticleSystem(float value)
+        {
+            if (fireParticleSystem != null && !fireParticleSystem.isPlaying)
+            {
+                Debug.Log("Playing particle");
+                fireParticleSystem.Play(true);
+            }
+
+            ChangeParticleEmission(value);
+        }
+
+        void StopFireParticleSystem()
+        {
+            if (fireParticleSystem != null && fireParticleSystem.isPlaying)
+            {
+                fireParticleSystem.Stop(true);
+            }
+        }
+
+        void ChangeParticleEmission(float value)
+        {
+            value = Mathf.Clamp01(value);
+            for (int i = 0; i < fireParticles.Length - 1; i++)
+            {
+                ParticleSystem fire = fireParticles[i];
+                var em = fire.emission;
+                em.rateOverTime = Mathf.FloorToInt(value * fireEmissionRateArray[i]);
+            }
+        }
+
+       
 
         void SetStartBurning(float value)
         {
@@ -80,6 +298,7 @@ namespace Kidnapped
             {
                 renderer.material.SetFloat("StartBurning", value);
             }
+    
         }
 
         void SetStopBurning(float value)
@@ -88,6 +307,15 @@ namespace Kidnapped
             {
                 renderer.material.SetFloat("StopBurning", value);
             }
+        }
+
+
+        public void StartBurning()
+        {
+            ResetBurningShaders();
+            SetStartBurning(0);
+            looping = true;
+            elapsed = 0;
         }
     }
 
